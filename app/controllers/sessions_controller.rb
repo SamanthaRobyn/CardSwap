9d1e9fe58a1dfd0db7da70f381eb8ff9d09a59c0
Samantha Samuels <your_email@whatever.com>
2014-12-01 19:45:13 -0800

    added background and fixed login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
 
     def create
         # Find the user that is trying to log in        
-        u = User.where(email: params[:user][:email]).first
-        if u && u.authenticate(params[:user][:password])
+        user = User.where(email: params[:user][:email]).first
+        if user && user.authenticate(params[:user][:password])
             # Store as a cookie in the users' browser the ID of them,
             # indicating that they are logged in
             # Saves long hexidecimal ID gobbldi password
-            session[:user_id] = u.id.to_s
+            session[:user_id] = user.id.to_s
             redirect_to businesscards_path
         else
             # Go back to the login page
